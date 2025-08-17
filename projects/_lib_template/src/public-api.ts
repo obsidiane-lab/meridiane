b3a659f065b0dd246a364025f4e5b9ee75484f49
blackbird <t.morabire@eventorizon.studio>
2025-08-17 23:49:35 +0200

    add ports on public

--- projects/_lib_template/src/public-api.ts
+++ projects/_lib_template/src/public-api.ts
@@ -1,6 +1,7 @@
 export * from './lib/provide-bridge';
 export * from './lib/tokens';
-export * from "./lib/facades/facade.factory"
-export * from "./lib/facades/resource.facade"
+export * from './lib/facades/facade.factory';
+export * from './lib/facades/resource.facade';
+export * from './lib/ports/resource-repository.port';
 
 export * from './models';
